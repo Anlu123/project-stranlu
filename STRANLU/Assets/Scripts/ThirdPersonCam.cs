d3723a644891990114824642446df08c85b19c44
FrancoBaezUni <icepandagames@gmail.com>
2023-06-23 01:32:05 -0300

    ThirdPerson

--- STRANLU/Assets/Scripts/ThirdPersonCam.cs
+++ STRANLU/Assets/Scripts/ThirdPersonCam.cs
@@ -12,6 +12,15 @@ public class ThirdPersonCam : MonoBehaviour
 
     public float rotationSpeed;
 
+    public Transform combatLookAt;
+
+    public CameraStyle currentStyle;
+    public enum CameraStyle
+    {
+        Basic,
+        Combat
+    }
+
     // Start is called before the first frame update
     private void Start()
     {
@@ -27,11 +36,25 @@ public class ThirdPersonCam : MonoBehaviour
         orientation.forward = viewDir.normalized;
 
         // Rotate player object
+
+        if (currentStyle == CameraStyle.Basic)
+        {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
         Vector3 inputDir = orientation.forward * verticalInput + orientation.right * horizontalInput;
 
         if (inputDir != Vector3.zero)
             playerObj.forward = Vector3.Slerp(playerObj.forward, inputDir.normalized, Time.deltaTime * rotationSpeed);
+        }
+
+        else if(currentStyle == CameraStyle.Combat) 
+        {
+            Vector3 dirToCombatLookAt = combatLookAt.position - new Vector3(transform.position.x, combatLookAt.position.y, transform.position.z);
+            orientation.forward = dirToCombatLookAt.normalized;
+
+            playerObj.forward = dirToCombatLookAt.normalized;
+
+        }
+
     }
 }
