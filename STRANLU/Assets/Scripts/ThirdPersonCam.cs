fa4e1d6f9223d08c9a656e52d901327a3f1229cd
FrancoBaezUni <icepandagames@gmail.com>
2023-06-23 21:02:06 -0300

    ALFIN 3ra persona VISION

--- STRANLU/Assets/Scripts/ThirdPersonCam.cs
+++ STRANLU/Assets/Scripts/ThirdPersonCam.cs
@@ -12,15 +12,6 @@ public class ThirdPersonCam : MonoBehaviour
 
     public float rotationSpeed;
 
-    public Transform combatLookAt;
-
-    public CameraStyle currentStyle;
-    public enum CameraStyle
-    {
-        Basic,
-        Combat
-    }
-
     // Start is called before the first frame update
     private void Start()
     {
@@ -35,26 +26,15 @@ public class ThirdPersonCam : MonoBehaviour
         Vector3 viewDir = player.position - new Vector3(transform.position.x, player.position.y, transform.position.z);
         orientation.forward = viewDir.normalized;
 
-        // Rotate player object
 
-        if (currentStyle == CameraStyle.Basic)
-        {
+        // Rotate player object
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
         Vector3 inputDir = orientation.forward * verticalInput + orientation.right * horizontalInput;
 
-        if (inputDir != Vector3.zero)
+        if(inputDir!= Vector3.zero)
+        {
             playerObj.forward = Vector3.Slerp(playerObj.forward, inputDir.normalized, Time.deltaTime * rotationSpeed);
         }
-
-        else if(currentStyle == CameraStyle.Combat) 
-        {
-            Vector3 dirToCombatLookAt = combatLookAt.position - new Vector3(transform.position.x, combatLookAt.position.y, transform.position.z);
-            orientation.forward = dirToCombatLookAt.normalized;
-
-            playerObj.forward = dirToCombatLookAt.normalized;
-
-        }
-
     }
 }
