f955f3b18ab4feb250bb355b52f6ca476003ac29
IStringer123 <francobattaglini123@gmail.com>
2023-08-08 00:13:08 -0300

    intento de arreglo
    xd

--- STRANLU/Assets/Scripts/PlayerMovement1.cs
+++ STRANLU/Assets/Scripts/PlayerMovement1.cs
@@ -22,6 +22,7 @@ public class PlayerMovement1 : MonoBehaviour
     float gravedad = -20f;
     Vector3 velocity;
     bool tocaPiso;
+    bool enAire;
 
     [Header("Deteccion suelo animacion")]
     public float distanciaDeteccion;
@@ -40,44 +41,47 @@ public class PlayerMovement1 : MonoBehaviour
     {
         tocaPiso = Physics.CheckSphere(detectaPiso.position, distanciaPiso, mascaraPiso);
 
-        if (tocaPiso)
-        {
-            anim.SetBool("salto", false);
-            lastTimeTouchedFloor = Time.time;
-        }
-
         if (tocaPiso && velocity.y < 0)
         {
             velocity.y = -2f;
+
         }
 
-        if (!tocaPiso)
+        if (tocaPiso)
+        {
+            enAire = false;
+            anim.SetBool("saltando", false);
+            anim.SetBool("aterrizando", false);
+        }
+
+        else if (!tocaPiso)
         {
             Debug.DrawRay(transform.position, -transform.up * distanciaDeteccion, Color.red);
-
+            enAire = true;
             RaycastHit hit;
-            if (Time.time - lastTimeTouchedFloor >= tiempoMinimoAnim && Physics.Raycast(transform.position, -transform.up, out hit, distanciaDeteccion, mascaraPiso))
+
+            if (Physics.Raycast(transform.position, -transform.up, out hit, distanciaDeteccion, mascaraPiso))
             {
-                Debug.Log("Aterrizando:  " + distanciaDeteccion);
-                anim.SetInteger("saltos", 1); //Aterriza
+                anim.SetBool("aterrizando", true);
+                anim.SetBool("saltando", true);
             }
         }
 
         if (Input.GetButtonDown("Jump") && tocaPiso)
         {
+            enAire = true;
+            anim.SetBool("saltando", true);
             velocity.y = Mathf.Sqrt(alturaDeSalto * -2 * gravedad);
-            anim.SetBool("salto", true);
-            anim.SetInteger("saltos", 0);
         }
-
-        velocity.y += gravedad * Time.deltaTime;
+                velocity.y += gravedad * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
 
         float x = Input.GetAxisRaw("Horizontal");
         float z = Input.GetAxisRaw("Vertical");
         Vector3 direccion = new Vector3(x, 0, z).normalized;
 
-        if (direccion.magnitude <= 0 && tocaPiso)
+        //Idle
+        if (!enAire && direccion.magnitude <= 0 && tocaPiso)
         {
             anim.SetFloat("movimientos", 0, 0.1f, Time.deltaTime);
         }
@@ -88,12 +92,13 @@ public class PlayerMovement1 : MonoBehaviour
             Vector3 move = orientation.forward * z + orientation.right * x;
 
             //Corriendo
-            if (Input.GetKey(KeyCode.LeftShift))
+            if (!enAire && Input.GetKey(KeyCode.LeftShift))
             {
                 controller.Move(move * velCorriendo * Time.deltaTime);
                 anim.SetFloat("movimientos", 1, 0.1f, Time.deltaTime);
             }
-            else //Caminando
+            // Caminando
+            else if (!enAire) 
             {
                 controller.Move(move * velocidad * Time.deltaTime);
                 anim.SetFloat("movimientos", 0.5f, 0.1f, Time.deltaTime);
